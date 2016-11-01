c0889edadbca4d2e26c217703fc02abc0ab791b5
ana <ana>
2016-11-01 15:16:11 -0200

    trocar

---
+++ trocar.hs
@@ -0,0 +1,8 @@
+trocar :: Integer -> [Integer]
+trocar a
+  | a == 0 = []
+  | a >= 100 = trocar (a-100) ++ [100]
+  | a >= 50  = trocar  (a-50) ++ [50]
+  | a >= 10  = trocar  (a-10) ++ [10]
+  | a >= 5   = trocar   (a-5) ++ [5]
+  | a >= 1   = trocar   (a-1) ++ [1] 
